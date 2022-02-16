2f1cc4119e5b768d6e2f1e484dddb8672d08f408
heredia lucas <heredialucasfac22@hotmail.com>
2022-02-16 10:22:54 -0300

    Haciendo sort

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,86 +4,108 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 // import { popularProducts } from "../data";
 import Product from "../Product/Product";
 import axios from "axios";
-import { Container, ContainerBtn, ContainerProd, Button, Arrow } from "./Products.style";
+import { Container, ContainerProd, Arrow } from "./Products.style";
 
 const Products = ({ cat, filters, sort }) => {
-    const [products, setProducts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [productForPage] = useState(8);
-    const indexLast = page * productForPage;
-    const indexFirst = indexLast - productForPage;
-    const allProductsForPage = products.slice(indexFirst, indexLast);
-    const allProducts = products.length;
-    let pagesNumber = [];
+  console.log(sort);
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [productForPage] = useState(8);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  console.log(filteredProducts);
+  const indexLast = page * productForPage;
+  const indexFirst = indexLast - productForPage;
+  const allProductsForPage = products.slice(indexFirst, indexLast);
+  const allProducts = products.length;
+  let pagesNumber = [];
 
-    function onPage(value) {
-        setPage(value);
+  filteredProducts.sort((a, b) => {
+    if ( sort === "desc" && b.price > a.price) {
+      return 1;
+    } else if ( sort === "desc" && b.price < a.price) {
+      return -1;
+    } else {
+      return 0;
     }
-
-    for (let i = 1; i <= Math.ceil(allProducts / productForPage); i++) {
-        pagesNumber.push(i);
+  });
+  filteredProducts.sort((a, b) => {
+    if (sort === "asc" && a.price > b.price) {
+      return 1;
+    } else if (sort === "asc" && a.price < b.price) {
+      return -1;
+    } else {
+      return 0;
     }
+  });
 
-    const handleClickPrev = () => {
-        if (page !== 1) setPage(page - 1)
-    }
+  function onPage(value) {
+    setPage(value);
+  }
 
-    const handleClickNext = () => {
-        if (page !== Math.ceil(allProducts / productForPage)) setPage(page + 1)
-    }
+  for (let i = 1; i <= Math.ceil(allProducts / productForPage); i++) {
+    pagesNumber.push(i);
+  }
 
+  const handleClickPrev = () => {
+    if (page !== 1) setPage(page - 1);
+  };
 
-    const [filteredProducts, setFilteredProducts] = useState([]);
+  const handleClickNext = () => {
+    if (page !== Math.ceil(allProducts / productForPage)) setPage(page + 1);
+  };
 
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const res = await axios.get(
-                    cat
-                        ? `http://localhost:8000/api/products?category=${cat}`
-                        : "http://localhost:8000/api/products"
-                );
-                setProducts(res.data)
-            } catch (err) { }
-        };
-        getProducts();
-    }, [cat]);
-    useEffect(() => {
-        cat &&
-            setFilteredProducts(
-                products.filter((item) =>
-                    Object.entries(filters).every(([Key, value]) =>
-                        item[Key].includes(value)
-                    )
-                )
-            );
-    }, [products, cat, filters]);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:8000/api/products?category=${cat}`
+            : "http://localhost:8000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([Key, value]) =>
+            item[Key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
-
-    return (
-        <Container>
-            <ContainerProd>
-                <Arrow>
-                    <ArrowLeftOutlined onClick={() => handleClickPrev()} />
-                </Arrow>
-                {pagesNumber.map((number) => {
-                    return <Arrow key={number} onClick={() => onPage(number)}>{number}</Arrow>
-                })}
-                <Arrow>
-                    <ArrowRightOutlined onClick={() => handleClickNext()} />
-                </Arrow>
-            </ContainerProd>
-            <ContainerProd>
-                {cat
-                    ? filteredProducts.map((item) => (
-                        <Product item={item} key={item.id} />
-                    ))
-                    : allProductsForPage.map((item) => (
-                        <Product item={item} key={item.id} />
-                    ))}
-            </ContainerProd>
-        </Container>
-    );
+  return (
+    <Container>
+      <ContainerProd>
+        <Arrow>
+          <ArrowLeftOutlined onClick={() => handleClickPrev()} />
+        </Arrow>
+        {pagesNumber.map((number) => {
+          return (
+            <Arrow key={number} onClick={() => onPage(number)}>
+              {number}
+            </Arrow>
+          );
+        })}
+        <Arrow>
+          <ArrowRightOutlined onClick={() => handleClickNext()} />
+        </Arrow>
+      </ContainerProd>
+      <ContainerProd>
+        {cat
+          ? filteredProducts.map((item) => (
+              <Product item={item} key={item.id} />
+            ))
+          : allProductsForPage.map((item) => (
+              <Product item={item} key={item.id} />
+            ))}
+      </ContainerProd>
+    </Container>
+  );
 };
 
 export default Products;
